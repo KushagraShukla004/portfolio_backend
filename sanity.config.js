7d9f204ad8ecea5aecd0fd3ec964c88e67e5f915
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-01-22 01:11:05 +0530

    Initial Commit

--- sanity.config.js
+++ sanity.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import schemas from './schemas/schema'
 
 export default defineConfig({
   name: 'default',
@@ -11,8 +11,28 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
-
+  tools: (prev) => {
+    // 👇 Uses environment variables set by Vite in development mode
+    if (import.meta.env.DEV) {
+      return prev
+    }
+    return prev.filter((tool) => tool.name !== 'vision')
+  },
   schema: {
-    types: schemaTypes,
+    types: schemas,
+  },
+  document: {
+    newDocumentOptions: (prev, {creationContext}) => {
+      if (creationContext.type === 'global') {
+        return prev.filter((templateItem) => templateItem.templateId != 'settings')
+      }
+      return prev
+    },
+    actions: (prev, {schemaType}) => {
+      if (schemaType === 'settings') {
+        return prev.filter(({action}) => !['unpublish', 'delete', 'duplicate'].includes(action))
+      }
+      return prev
+    },
   },
 })
