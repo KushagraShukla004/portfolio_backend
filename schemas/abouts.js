6bccfa120917a84934b4922563ecdbea50c33617
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-06-12 13:13:00 +0530

    minor change

--- schemas/abouts.js
+++ schemas/abouts.js
@@ -1,26 +1,30 @@
-export default{
-    name:'abouts',
-    title:'Abouts',
-    type: 'document',
-    fields:[
-        {
-            name:'title',
-            title:'Title',
-            type:'string'
-        },
-        {
-            name:'description',
-            title:'Description',
-            type:'string'
-        },
-        {
-            name:'imgUrl',
-            title:'ImgUrl',
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-        },
-        
-    ]
+export default {
+  name: 'abouts',
+  title: 'Abouts',
+  type: 'document',
+  fields: [
+    {
+      name: 's_no',
+      title: 'S_no',
+      type: 'number',
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'imgUrl',
+      title: 'ImgUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
 }
