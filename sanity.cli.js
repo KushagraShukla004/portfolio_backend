83642545c3a00f137db28352207fa9e7cfe0cdb7
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2024-06-25 16:07:07 +0530

    minor change

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'da7xkf3u',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
